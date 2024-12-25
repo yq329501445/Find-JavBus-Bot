85835ab2dff351ef1ada4b0605598f5464cf6d96
yq329501445 <94464575+yq329501445@users.noreply.github.com>
2024-12-26 04:05:55 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,20 @@
-export const ALLOWED_GROUPS = []
-export const BOT_TOKEN = '7902050960:AAHbzct5IOmieS1RRqq5naOSbjQLYLygRmE'
-export const ROBOT_NAME = '@avmenubot'
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
 
+const ALLOWED_GROUPS = []
+const BOT_TOKEN = '7902050960:AAHbzct5IOmieS1RRqq5naOSbjQLYLygRmE'
+const ROBOT_NAME = '@avmenubot'
+
+async function handleRequest(request) {
+  // 你的代码逻辑
+  return new Response('Hello worker!', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
+
+export {
+  ALLOWED_GROUPS,
+  BOT_TOKEN,
+  ROBOT_NAME
+}
