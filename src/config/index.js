cbc2e9f3e4c85d8fd6e9bc8254f9558b43f27a50
yq329501445 <94464575+yq329501445@users.noreply.github.com>
2024-12-25 23:57:28 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const ALLOWED_GROUPS = []
 export const BOT_TOKEN = '7902050960:AAHbzct5IOmieS1RRqq5naOSbjQLYLygRmE'
-export const ROBOT_NAME = 'avmenu'
+export const ROBOT_NAME = '@avmenubot'
